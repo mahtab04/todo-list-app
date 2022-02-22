b822e0c9ff488969cb718932ae73738c6b217cad
Mahtab Alam <smmalam9@gmail.com>
2022-02-22 20:15:00 +0530

    added .env and procfile

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 5000;
 const db = require("./config/mongoose");
 const Task = require("./models/task");
 
